c11cbc1ef9b94e737d81f7d54ab24b24d57ec752
cesarluilly <cesarluilly.garcia@hotmail.com>
2024-05-04 09:05:07 -0600

    Tarea Funcionalidad Editar Slider

--- BlogCore/BlogCore/Areas/Admin/Controllers/SlidersController.cs
+++ BlogCore/BlogCore/Areas/Admin/Controllers/SlidersController.cs
@@ -74,6 +74,78 @@ namespace BlogCore.Areas.Admin.Controllers
             return View(slider);
         }
 
+        //--------------------------------------------------------------------------------------------------------------
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+
+            if (id != null)
+            {
+                var slider = _unitOfWork.SliderRepo.Get(id.GetValueOrDefault());
+                return View(slider);
+            }
+
+            return View();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Slider slider)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                var sliderDdesdeBd = _unitOfWork.SliderRepo.Get(slider.Id);
+
+                if (archivos.Count() > 0)
+                {
+                    //Nuevo imagen slider
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\sliders");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+
+                    //var nuevaExtension = Path.GetExtension(archivos[0].FileName);
+
+                    var rutaImagen = Path.Combine(rutaPrincipal, sliderDdesdeBd.UrlImagen.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(rutaImagen))
+                    {
+                        System.IO.File.Delete(rutaImagen);
+                    }
+
+                    //Nuevamente subimos el archivo
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+
+
+                    slider.UrlImagen = @"\imagenes\sliders\" + nombreArchivo + extension;
+
+                    _unitOfWork.SliderRepo.Update(slider);
+                    _unitOfWork.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    //Aquí sería cuando la imagen ya existe y se conserva
+                    slider.UrlImagen = sliderDdesdeBd.UrlImagen;
+                }
+
+                _unitOfWork.SliderRepo.Update(slider);
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(slider);
+        }
+
         #region Llamadas a la API (SERVICIOS DE BACKEND DONDE SOLO SE RETORNA INFO A TRAVES DE JSON)
         //--------------------------------------------------------------------------------------------------------------
         [HttpGet]
